5d518a8099cbaec2e8cfe2e6e69c2a15be33894c
KristianZ <2193492+KristianZ@users.noreply.github.com>
2025-09-03 17:26:32 -0700

    Add API request headers for improved authentication and tracking

--- src/DweloAPI.ts
+++ src/DweloAPI.ts
@@ -132,6 +132,10 @@ export class DweloAPI {
       headers: {
         ...headers,
         Authorization: `Token ${this.token} `,
+        Host: 'api.dwelo.com',
+        Origin: 'https://app.dwelo.com',
+        Referer: 'https://app.dwelo.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       },
     });
     return response;
